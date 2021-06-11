81b62156f77dae519201322e78aee78131e17baf
Shir Shrem <shirshrem@mail.tau.ac.il>
2021-06-11 22:30:17 +0300

    add home

---
+++ client/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = () => {
+  const handleClick = () => {
+    console.log("hello");
+  };
+
+  return <div className="home"></div>;
+};
+
+export default Home;
