e4f276cf8777b9b642ca7174f4297bbfc36a047a
Shir Shrem <shirshrem@mail.tau.ac.il>
2021-08-06 20:34:00 +0300

    fixed the new movies feature on rmg

--- client/src/RandomMovieGenerator.js
+++ client/src/RandomMovieGenerator.js
@@ -4,29 +4,32 @@ import { Button } from "@material-ui/core";
 import RandomMoviePreferences from "./RandomMoviePreferences";
 import ChooseGeners from "./ChooseGenres";
 import React from "react";
+import Math from "Math";
 
 const API_KEY = "e411d2086029c6a6bb841f8bbfa4fd18";
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
 const currentPreferencesToSearch = (genres, preferences, page) => {
   let searchTerm =
     "https://api.themoviedb.org/3/discover/movie?api_key=" +
     API_KEY +
+    "&language=en-US" +
+    "&sort_by=vote_count.desc" +
     "&with_genres=" +
     genres.map((genre) => genre.id).join(",");
 
-  if (preferences.newMovies) searchTerm += "&release_date.gte=2019";
-  if (preferences.topRated) searchTerm += "&vote_average.gte=9";
+  if (preferences.newMovies)
+    searchTerm += "&primary_release_date.gte=2019-01-01";
+  if (preferences.topRated) searchTerm += "&vote_average.gte=8.5";
   if (preferences.lessthan2) searchTerm += "&with_runtime.lte=120";
   searchTerm += "&page=" + page;
   console.log(searchTerm);
 
   return searchTerm;
 };
-const sideEffect = (f) => (x) => {
-  f(x);
-  return x;
-};
-
-const log = sideEffect(console.log);
 const RandomMovieGenerator = ({ myList, setMyList }) => {
   const [movies, setMovies] = useState(null); // random movies list
   const [genres, setGenres] = useState([]); // list of chosen geners
@@ -41,14 +44,12 @@ const RandomMovieGenerator = ({ myList, setMyList }) => {
   const [loading, setLoading] = useState(false);
 
   const getXFirstPages = async (x) => {
-    return log(
-      await Promise.all(
-        Array(x)
-          .fill(1)
-          .map((_, y) => y + 1)
-          .map(getPage)
-          .map((url) => fetch(url))
-      )
+    return Promise.all(
+      Array(x)
+        .fill(1)
+        .map((_, y) => y + 1)
+        .map(getPage)
+        .map((url) => fetch(url))
     );
   };
 
@@ -83,6 +84,7 @@ const RandomMovieGenerator = ({ myList, setMyList }) => {
       >
         Generate!
       </Button>
+
       <OutputList movies={movies} myList={myList} setMyList={setMyList} />
     </div>
   );
